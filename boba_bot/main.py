2b83165df2440bebfdcae39e72752f390b55a81a
benchan777 <benchan777@gmail.com>
2021-03-16 17:37:29 -0700

    rewrote all functions to use discord.ext commands

--- boba_bot/main.py
+++ boba_bot/main.py
@@ -4,12 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from yelpapi import YelpAPI
 import discord
+from discord.ext import commands
 import os
 
 load_dotenv()
 yelp_api = YelpAPI(os.getenv('yelp_api_key'), timeout_s = 3.0)
 
-client = discord.Client()#Initialize discord client so bot can be logged in
+bot = commands.Bot(command_prefix = '$', intents = discord.Intents.all()) #Initialize discord bot
 engine = create_engine('sqlite:///database.db') #Create SQLAlchemy engine
 Base.metadata.create_all(engine) #Create database tables
 Session = sessionmaker(engine) #Define Session class
@@ -23,71 +24,62 @@ def remove_first_word(string):
     new_string = ' '.join(omit_first_word)
     return new_string
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
+    print(f"Logged in as {bot.user}")
 
-#Functions to be triggered depending on user input
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command(pass_context = True)
+async def test(ctx):
+    print(ctx.message.author)
+    print(ctx.author.id)
+    new_user = User(
+        user_id = ctx.message.author.id,
+        username = f"{ctx.message.author.name}#{ctx.message.author.discriminator}"
+    )
+    db.add(new_user)
+    db.commit()
+
+@bot.command(pass_context = True)
+async def location(ctx, *args):
+    location = ' '.join(args)
+    user = db.query(User).filter_by(user_id = ctx.message.author.id).one()
+    user.location = location
+    db.commit()
+
+    for new_location in db.query(User.location).filter_by(user_id = ctx.message.author.id):
+        await ctx.send(f"{new_location[0]} has been set as {ctx.message.author.mention}'s location!")
+
+@bot.command(pass_context = True)
+async def boba(ctx, *args):
+    for i in db.query(User.location).filter_by(user_id = ctx.message.author.id):
+        location = i
+
+    if location[0] is None:
+        location = ' '.join(args)
+        result = yelp_api.search_query(term = 'boba', location = location, categories = 'Bubble Tea')
+        store_info = result['businesses']
+
+        store_names = []
+        for store in store_info:
+            store_names.append(store['name'])
+
+        print('using user specified location')
+        await ctx.send(f"Displaying stores in {location}: {store_names}")
     
-    if message.content.startswith('$test'):
-        # print(message.author)
-        # print(message.author.id)
-        # new_user = User(
-        #     user_id = message.author.id,
-        #     username = f"{message.author.name}#{message.author.discriminator}"
-        # )
-        # db.add(new_user)
-        # db.commit()
-        pass
+    else:
+        # stored_location = db.query(User.location).filter_by(user_id = message.author.id)
+        result = yelp_api.search_query(term = 'boba', location = location, categories = 'Bubble Tea')
+        store_info = result['businesses']
 
-    #Displays a list of boba shops near user's entered location
-    if message.content.startswith('$boba'):
-        print(message.author)
-        print(message.author.id)
-        new_user = User(
-            user_id = message.author.id,
-            username = f"{message.author.name}#{message.author.discriminator}"
-        )
-        db.add(new_user)
-        db.commit()
-
-        for i in (db.query(User.location).filter_by(user_id = message.author.id)):
-            location = i
-
-        if location[0] is None:
-            location = remove_first_word(message.content)
-            result = yelp_api.search_query(term = 'boba', location = location, categories = 'Bubble Tea')
-            store_info = result['businesses']
-
-            store_names = []
-            for store in store_info:
-                store_names.append(store['name'])
-
-            print('using user specified location')
-            await message.channel.send(f"Displaying stores in {location}: {store_names}")
+        store_names = []
+        for store in store_info:
+            store_names.append(store['name'])
         
-        else:
-            # stored_location = db.query(User.location).filter_by(user_id = message.author.id)
-            stored_result = yelp_api.search_query(term = 'boba', location = location, categories = 'Bubble Tea')
-            stored_store_info = stored_result['businesses']
+        print('using stored location')
+        await ctx.send(f"Your stored location is {location[0]}!\nDisplaying boba stores in {location[0]}:\n{store_names}")
 
-            stored_store_names = []
-            for store in stored_store_info:
-                stored_store_names.append(store['name'])
-            
-            print('using stored location')
-            await message.channel.send(f"Your stored location is {location[0]}! Displaying boba stores in {location[0]}:\n{stored_store_names}")
-            
-    #Allow user to save their current location
-    if message.content.startswith('$location'):
-        location = remove_first_word(message.content)
-        user = db.query(User).filter_by(user_id = message.author.id).one()
-        user.location = location
-        db.commit()
-
-        for new_location in db.query(User.location).filter_by(user_id = message.author.id):
-            await message.channel.send(f"{new_location[0]} has been set as {message.author.mention}'s location!")
+# #Functions to be triggered depending on user input
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
