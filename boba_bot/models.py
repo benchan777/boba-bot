3340eec7b6498962c05311efc7d14508b8336685
benchan777 <benchan777@gmail.com>
2021-03-19 02:06:03 -0700

    create many to many relationship table to join user & bobashop tables

--- boba_bot/models.py
+++ boba_bot/models.py
@@ -1,7 +1,13 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+association_table = Table('association', Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('boba_store_id', Integer, ForeignKey('boba_store.id'))
+)
 
 class User(Base):
     __tablename__ = 'users'
@@ -12,11 +18,21 @@ class User(Base):
     location = Column(String(200))
     server_id = Column(String(200))
     server_name = Column(String(200))
+    user_order = relationship(
+        'BobaShop',
+        secondary = association_table,
+        back_populates = 'user_with_order'
+    )
 
 class BobaShop(Base):
     __tablename__ = 'boba_store'
 
     id = Column(Integer, primary_key = True)
-    name = Column(String(200))
     phone = Column(String(200))
+    name = Column(String(200))
     city = Column(String(200))
+    user_with_order = relationship(
+        'User',
+        secondary = association_table,
+        back_populates = 'user_order'
+    )
