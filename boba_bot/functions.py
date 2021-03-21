27e1dbd4ab250e15ca9be299c590438a38057408
benchan777 <benchan777@gmail.com>
2021-03-20 22:31:44 -0700

    fix for inconsistent yelp api response (empty key value pairs)

--- boba_bot/functions.py
+++ boba_bot/functions.py
@@ -7,9 +7,13 @@ from discord.ext import commands
 def store_info_embed(name, url, description, thumbnail, rating, price, phone):
     embed = discord.Embed(title = name, url = url, description = description, color = 0x7b00ff)
     embed.set_thumbnail(url = thumbnail)
-    embed.add_field(name = 'Rating', value = rating, inline = True)
-    embed.add_field(name = 'Price', value = price, inline = True)
-    embed.add_field(name = 'Phone Number', value = phone, inline = True)
+
+    embed_1 = 'N/A' if len(str(rating)) == 0 else rating
+    embed_2 = 'N/A' if len(str(price)) == 0 else price
+    embed_3 = 'N/A' if len(str(phone)) == 0 else phone
+    embed.add_field(name = 'Rating', value = embed_1, inline = True)
+    embed.add_field(name = 'Price', value = embed_2, inline = True)
+    embed.add_field(name = 'Phone Number', value = embed_3, inline = True)
     return embed
 
 #Function to save searched store information into database
